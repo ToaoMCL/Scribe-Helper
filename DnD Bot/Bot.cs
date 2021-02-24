70ad97bba035ed245d6d555104924e6276c6a894
Eryk-Pabianczyk <unconfigured@null.spigotmc.org>
2021-02-24 18:43:54 +0000

    Add project files.

---
+++ DnD Bot/Bot.cs
@@ -0,0 +1,73 @@
+﻿
+using DSharpPlus;
+using DSharpPlus.Interactivity;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace DnD_Bot
+{
+    class Bot
+    {
+        public static DiscordClient Client { get; private set; }
+        public InteractivityModule Interactivity { get; private set; }
+        public CommandsNextModule Commands { get; private set; }
+
+
+        public async Task RunAync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            {
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                {
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            Client.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromHours(36)
+            });
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefix = configJson.Prefix,
+                EnableDms = true,
+                EnableMentionPrefix = true,
+                CaseSensitive = false,
+                IgnoreExtraArguments = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<EntertainmentCommands>();
+            
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DnD Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnD_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
